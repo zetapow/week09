c7d59fd51d917c9d6837d0ec093e6d2d68f997e2
peter-zhou <zetapow@gmail.com>
2025-03-31 11:17:51 +1300

    monday http

---
+++ httpMod/server.js
@@ -0,0 +1,46 @@
+// import http module
+const http = require("http");
+
+const userList = [
+   { name: "andy", age: 21 },
+   { name: "randy", age: 27 },
+   { name: "sandy", age: 36 },
+];
+
+// createServer
+// can write as
+/* http.createServer(function(req,res)) {} */
+const server = http.createServer((req, res) => {
+   console.log("request received");
+   console.dir(req);
+   console.log("requested URL: ", req.url);
+   if (req.url === "/") {
+      res.write("<h1>Response write</h1>");
+      res.end("bye");
+   }
+   if (req.url === "/favicon.ico") {
+      res.writeHead(204, { "Content-Type": " image/x-icon" });
+      res.end();
+   }
+   if (req.url === "/api/greet") {
+      res.write(
+         "<div><h1>Hello, thank you for visiting api/greet</h1><p>Looks like its more html</p></div>"
+      );
+      res.end("end of greet");
+   }
+   if (req.url === "/api/userlist") {
+      // Cannot send js objects or arrays. Only string in json format
+      res.write(JSON.stringify(userList));
+      res.end();
+   }
+});
+
+/* takes callback function- 2 params (request, response)
+represent incoming request and outgoing response
+
+Function called for each incoming request. Can define how server should handle requests
+*/
+const PORT = 3000;
+server.listen(PORT, () => {
+   console.log(`Listening on port ${PORT}`);
+});
